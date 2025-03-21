d5ec70f10577bb811337e33c5a05b3737eb30c02
Shees Usman <shees@nusoft.co>
2025-03-21 14:42:28 +0500

    Added server start command and port 80 default

--- json-extractor-server.js
+++ json-extractor-server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 
 // Create Express app
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 
 // Configure middleware
 app.use(cors()); // Enable CORS for all routes
